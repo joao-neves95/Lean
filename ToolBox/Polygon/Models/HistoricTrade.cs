b5a4af2af9fc1bc12b63824e2038b2d369165f17
João Neves <joao95neves@gmail.com>
2020-06-28 20:14:11 +0100

    [ToolBox/Polygon/Models/]: Refactored HistoricTrade (read desc)
    - Refactored HistoricTrade to use JsonProperty attribute to improve code readability.
    - Removed unnecessary properties.

--- ToolBox/Polygon/Models/HistoricTrade.cs
+++ ToolBox/Polygon/Models/HistoricTrade.cs
@@ -1,40 +1,54 @@
-﻿
+﻿/*
+ * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
+ * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * https://github.com/joao-neves95
+*/
+
+using Newtonsoft.Json;
+
 using QuantConnect.Data.Market;
 
 namespace QuantConnect.ToolBox.Polygon.Models
 {
-    public class HistoricTradeV2 : IPolygonAPIResult
+    public class HistoricTrade : IPolygonAPIResult
     {
-        public string T { get; set; }
+        [JsonProperty("t")]
+        public int Timestamp { get; set; }
 
-        public int t { get; set; }
+        [JsonProperty("i")]
+        public string TradeID { get; set; }
 
-        public int y { get; set; }
+        [JsonProperty("x")]
+        public int ExchangeID { get; set; }
 
-        public int f { get; set; }
+        [JsonProperty("s")]
+        public int Size { get; set; }
 
-        public int q { get; set; }
+        [JsonProperty("p")]
+        public decimal Price { get; set; }
 
-        public string i { get; set; }
-
-        public int x { get; set; }
-
-        public int s { get; set; }
-
-        public decimal p { get; set; }
-
-        public int z { get; set; }
-
-        public Tick ToTick()
+        public Tick ToTick(Symbol symbol = null)
         {
             return new Tick()
             {
-                Quantity = this.s,
-                Value = this.p,
-                Symbol = this.T,
-                Time = Time.UnixTimeStampToDateTime(this.t),
+                Quantity = this.Size,
+                Value = this.Price,
+                Time = Time.UnixTimeStampToDateTime(this.Timestamp),
+
                 DataType = MarketDataType.Tick,
-                TickType = TickType.Trade
+                TickType = TickType.Trade,
+                Symbol = symbol,
             };
         }
     }
