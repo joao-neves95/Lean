0280f82def2eaef36247b87c66b728f4639dff17
João Neves <joao95neves@gmail.com>
2020-07-02 18:17:47 +0100

    [ToolBox/Polygon/]: Set up PolygonDataQueueHandler

---
+++ ToolBox/Polygon/PolygonDataQueueHandler/PolygonDataQueueHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using QuantConnect.Data;
+using QuantConnect.Interfaces;
+using QuantConnect.Packets;
+
+namespace QuantConnect.ToolBox.Polygon.PolygonDataQueueHandler
+{
+    public class PolygonDataQueueHandler : IDataQueueHandler
+    {
+        public bool IsConnected => false;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<BaseData> GetNextTicks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Subscribe(LiveNodePacket job, IEnumerable<Symbol> symbols)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unsubscribe(LiveNodePacket job, IEnumerable<Symbol> symbols)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
